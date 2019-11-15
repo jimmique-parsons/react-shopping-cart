93a1bb91ddcf9ea94deb3b53ead1abad4da6873f
Jimmique <jimmique88@gmail.com>
2019-11-15 04:17:45 -0800

    imported CartContext into App.js and nested the component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ShoppingCart from './components/ShoppingCart';
 
 // Contexts
 import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 
 function App() {
  // data is an array of product objects with id, title, price, and image props
@@ -23,21 +24,22 @@ function App() {
 
 	return (
 	<ProductContext.Provider value={{ products, addItem }}>
-		<div className="App">
+    	<CartContext.Provider value={{ cart }}>
+			<div className="App">
 			<Navigation cart={cart} />
-
 			{/* Routes */}
 			<Route
 				exact
 				path="/"
-	component={ Products }
+				component={ Products }
 			/>
 
 			<Route
 				path="/cart"
 				render={() => <ShoppingCart cart={cart} />}
 			/>
-		</div>
+			</div>
+    	</CartContext.Provider>
     </ProductContext.Provider>
  	);
  }
